4212afc9aef379eceba6980508f9aa0727261420
unknown <qedapalisok@tip.edu.ph>
2025-09-10 02:35:44 +0800

    update

---
+++ Assets/XrSocketTagInteractor.cs
@@ -0,0 +1,20 @@
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+public class XrSocketTagInteractor : XRSocketInteractor
+{
+    public string targetTag;
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+    }
+    
+}
