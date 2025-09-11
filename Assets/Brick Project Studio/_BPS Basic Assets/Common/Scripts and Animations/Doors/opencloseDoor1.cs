dd0334d09ee0c7a7d2e848580c07ce65b5e8ca28
unknown <qedapalisok@tip.edu.ph>
2025-09-11 11:51:57 +0800

    update

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor1.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor1.cs
@@ -1,72 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-namespace SojaExiles
-
+public class XRDoor : MonoBehaviour
 {
-	public class opencloseDoor1 : MonoBehaviour
-	{
+    [SerializeField] private Animator animator;
+    private bool isOpen = false;
 
-		public Animator openandclose1;
-		public bool open;
-		public Transform Player;
-
-		void Start()
-		{
-			open = false;
-		}
-
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
-
-						}
-
-					}
-				}
-
-			}
-
-		}
-
-		IEnumerator opening()
-		{
-			print("you are opening the door");
-			openandclose1.Play("Opening 1");
-			open = true;
-			yield return new WaitForSeconds(.5f);
-		}
-
-		IEnumerator closing()
-		{
-			print("you are closing the door");
-			openandclose1.Play("Closing 1");
-			open = false;
-			yield return new WaitForSeconds(.5f);
-		}
-
-
-	}
+    // Called when you interact with the door (via XR Direct Interactor or Ray Interactor)
+    public void ToggleDoor()
+    {
+        if (isOpen)
+        {
+            animator.Play("Closing 1");
+            isOpen = false;
+        }
+        else
+        {
+            animator.Play("Opening 1");
+            isOpen = true;
+        }
+    }
 }
--- Assets/XrSocketTagInteractor.cs
+++ Assets/XrSocketTagInteractor.cs
@@ -1,8 +1,10 @@
+/*
 using System.Collections.ObjectModel;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
 public class XrSocketTagInteractor : XRSocketInteractor
 {
     public string targetTag;
@@ -16,5 +18,24 @@ public class XrSocketTagInteractor : XRSocketInteractor
     {
         return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
     }
-    
+}
+*/
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class XrSocketTagInteractor : XRSocketInteractor
+{
+    [SerializeField] private string targetTag = "Untagged";
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+    }
 }
